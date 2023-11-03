50c0455f81e5b3e887ecd9942f73ad6687cd5416
Fatih Şahin <94977459+ssahinfatih@users.noreply.github.com>
2023-11-03 15:40:11 +0300

    .

--- src/main/java/com/tobeto/spring/b/PersonsController.java
+++ src/main/java/com/tobeto/spring/b/PersonsController.java
@@ -2,30 +2,52 @@ package com.tobeto.spring.b;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController// Controller clası olduğunu belirtiyoruz ve o özellikleri tanımlıyor.
 @RequestMapping("api/persons") // Bu controllerın kontrol edeceği alt routeları tanımlamak
 public class PersonsController {
 
+    List<Person> inMemoryList= new ArrayList<>();
+
     @GetMapping // api/persons
-    public String get(){
-        return "Get";
-    }
-    @GetMapping("get2") // api/persons/get2
-    public String get2(){
-        return "Get 2";
-    }
-    @PostMapping
-    public String post(){
-        return "Tobeto";
+
+    public List<Person> get(){
+
+        return inMemoryList;
     }
 
-     @PutMapping
-    public  String put(){
-        return "put ";
+    //@GetMapping("{getById}")
+    // query string, path veriable
+    //path => https://localhost:8081/api/persons/1 => 1=id
+    //RequestParam-query string=> https://localhost:8081/api/persons/getById?id=1
+    @GetMapping("{id}")
+    public Person getById(@PathVariable int id){
+        //Lambda Expressions
+        //Stream API
+        Person person= inMemoryList
+                .stream()
+                .filter((p) -> p.getId() == id)
+                .findFirst()
+                .orElseThrow();
+        return person;
+    }
+
+    @PostMapping
+    public void add(@RequestBody Person person){
+        inMemoryList.add(person);
+
+    }
+
+     @PutMapping()
+    public  void update(@RequestBody Person person){
+         inMemoryList.add(person);
      }
 
-     @DeleteMapping
-    public  String delete(){
-        return "delete ";
+     @DeleteMapping("{id}")
+    public void delete(@PathVariable int id){
+        inMemoryList.remove(getById(id));
+
      }
 }
